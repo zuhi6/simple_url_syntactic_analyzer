16fab81ad93814c417f0d2ed1248a83734c474f6
Michal Zajic <zajicmichal6@gmail.com>
2019-11-14 20:16:14 +0100

    [feature] added trace table

--- bundle.js
+++ bundle.js
@@ -2,68 +2,112 @@
 let { table, rules } = require('./table_and_rules');
 const { transformTable } = require('./table_transform');
 
+
 table = transformTable(table);
 
 let url = '';
 
 const startRule = ["A"];
 
-checkUrls = () => {
-    
-    let urls = document.getElementById("urls").value.trim().split("|");
-    let table = document.getElementById("table")
-    const [ firstUrl ] = urls;
+let trace = [];
+let previousRules = [];
 
-    if(!firstUrl) {
-        table.style.display ="none";
+showTable = (trace) => {
+    let traceTable = document.getElementById("trace-table");
+   
+    trace = trace.replace(/\$/g,'$,').split(',').reduce((result, value, index, array) =>{
+        if (index % 2 === 0)
+          result.push(array.slice(index, index + 2));
+        return result;
+      }, []);
+
+    trace = [...new Set(trace.map(trace => trace.toString()))].map(distinct => distinct.split(","));
+    
+    let rows = ''
+
+    trace.forEach(([rule, url]) => {
+        if(!url) return;
+        rows += `<tr>
+                    <td width='184' height='52'>
+                    ${rule}
+                    </td>
+                    <td width='184' height='52'>
+                    ${url}
+                    </td>                    
+                 <tr>`
+    })
+    traceTable.style.display = "block";
+    document.getElementById("trace-tbody").innerHTML = rows;
+}
+
+checkUrls = () => {
+
+    let urls = document.getElementById("urls").value.trim().split("|").filter(url => url.length);
+    let table = document.getElementById("table")
+
+    document.getElementById("trace-table").style.display = "none";
+
+
+
+    const [firstUrl] = urls;
+
+    if (!firstUrl) {
+        table.style.display = "none";
         return;
     }
 
     urls = urls.map(urlItem => {
+        trace = [];
         url = `${urlItem}$`;
-        return {url:urlItem, result: iterateUrl(startRule)}
+        return { url: urlItem, result: iterateUrl(startRule), trace: trace}
     });
 
     let rows = ''
-
+    
     urls.forEach(url => {
+        const color = url.result ? 'lightgreen' : 'red';
         rows += `<tr>
                     <td width='184' height='52'>
-                    ${url.url}
+                    <mark style="background-color: ${color}">${url.url}<mark>
                     </td>
                     <td width='184' height='52'>
                     ${url.result}
                     </td>
+                    <td>
+						<input type=button value="Show Trace" onclick="showTable('${url.trace.join('')}')" style="width:100%">
+					</td>
+                    
                  <tr>`
     })
-   
-    table.style.display ="block";
+
+    table.style.display = "block";
     document.getElementById("tbody").innerHTML = rows;
-  
+
 }
 
 const iterateUrl = (ruleArr, previousFirstRule, regExpression) => {
-   
+
     //Get first nonterminal or terminal symbol from rule array
-    let [ [ firstNonTerm ] ] = ruleArr;
+    let [[firstNonTerm]] = ruleArr;
 
     //Get first character of the remaining url to by analyzed
-    let [ firstChar ] = url;
+    let [firstChar] = url;
 
+    trace.push([ruleArr.join(''), url]);
     //Check if they are equal if yes we are removing them from url and rule array
     if (firstNonTerm == firstChar) {
-       
+
         const tableObj = table[startRule][firstChar];
         const baseLength = (previousFirstRule == startRule) ? tableObj.base.length : 1;
-     
+
         //If the firstNonTerm character doesn't matche regexExpression '[A-Z]' we are cutting
         !firstNonTerm.match(new RegExp('[A-Z]')) && (ruleArr = ruleArr.join('').substr(baseLength).split(''))
-                                                 && (url = url.substr(baseLength));
-                                    
-    } else if (regExpression) { 
+            && (url = url.substr(baseLength));
+
+    } else if (regExpression) {
         // if there is regExpression present from last recursion we try to match the regex if true we are cutting 
         firstChar.match(regExpression) && ruleArr.shift() && (url = url.substr(1))
-        
+
     } else if (!firstNonTerm && ruleArr.length != 1) {
         //epsilon rule -> just remove empty element at the start and run recursion again
         ruleArr.shift();
@@ -73,10 +117,10 @@ const iterateUrl = (ruleArr, previousFirstRule, regExpression) => {
     /*need to run join.split everytime in case non of the above conditions are true,
      so if there is new rule non terminal symbols are seperated*/
     ruleArr = ruleArr.join('').split('');
-   
+
     //updating firstChar and firstNonTerm with new characters after cutting
-    [ firstChar ] = url;
-    [ firstNonTerm ] = ruleArr;
+    [firstChar] = url;
+    [firstNonTerm] = ruleArr;
 
     // if we hit the $ sign we check if there are no rules left if not we check for url length if there is only dollar sign left we return true
     if (firstChar == "$" && !ruleArr.length) {
@@ -89,13 +133,13 @@ const iterateUrl = (ruleArr, previousFirstRule, regExpression) => {
     }
 
     //applying new rule
-    const newRule = table[firstNonTerm][firstChar].rule ? rules[table[firstNonTerm][firstChar].rule] 
-                    : rules[table[firstNonTerm][firstChar]];
+    const newRule = table[firstNonTerm][firstChar].rule ? rules[table[firstNonTerm][firstChar].rule]
+        : rules[table[firstNonTerm][firstChar]];
 
-    //console.log(ruleArr, url, "3");
-    ruleArr.splice(0,1,newRule);
+    //updating first element of rules array               
+    ruleArr.splice(0, 1, newRule);
 
-    //check if new rule is regExpression if so we send it in recursion else we send it as null
+    //check if new rule is regular expression if so we send it in recursion else we send it as null
     regExpression = (newRule == '[A-Za-z]' || newRule == '[0-9]') ? new RegExp(newRule) : null
 
     return iterateUrl(ruleArr, firstNonTerm, regExpression)
